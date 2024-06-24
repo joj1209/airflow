5ddd834c4862e526f53d2b34dc6277b9961dd06f
bskim <joj1209@naver.com>
2024-06-24 16:15:12 +0900

    커넥션 테스트용 dag 업로드(수정)

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -3,7 +3,6 @@ import pendulum
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.empty import EmptyOperator
 
 with DAG(
     dag_id="dags_bash_operator",
@@ -26,40 +25,5 @@ with DAG(
         bash_command="echo $HOSTNAME",
     )
     
-    t1 = EmptyOperator(
-        task_id="t1"
-    )
-    
-    t2 = EmptyOperator(
-        task_id="t2"
-    )
-    
-    t3 = EmptyOperator(
-        task_id="t3"
-    )
-    
-    t4 = EmptyOperator(
-        task_id="t4"
-    )
-    
-    t5 = EmptyOperator(
-        task_id="t5"
-    )
-    
-    t6 = EmptyOperator(
-        task_id="t6"
-    )
-    
-    t7 = EmptyOperator(
-        task_id="t7"
-    )
-    
-    t8 = EmptyOperator(
-        task_id="t8"
-    )
-    
     bash_t1 >> bash_t2
     
-    t1 >> [t2,t3] >> t4
-    t5 >> t4
-    [t4,t7] >> t6 >> t8
