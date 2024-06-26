6ba8c4bb1594dec089fc3ead269b0e45dcd67da2
bskim <joj1209@naver.com>
2024-06-26 14:51:14 +0900

    공통 외부함수 추가

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,21 @@
+import datetime
+import pendulum
+import random
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from common.common_func import get_stfp
+
+with DAG(
+    dag_id="dags_python_import_func",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+    # tags=["example", "example2"],
+    # params={"example_key": "example_value"},
+) as dag:
+
+    task_get_stfp = PythonOperator(
+        task_id = 'task_get_stfp',
+        python_callable=get_stfp
+    )
