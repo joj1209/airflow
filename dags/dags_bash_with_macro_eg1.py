07cc3f6af94ea1028705671ffad0702785e18f8f
bskim <joj1209@naver.com>
2024-06-27 15:48:16 +0900

    bash macro

---
+++ dags/dags_bash_with_macro_eg1.py
@@ -0,0 +1,26 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg1",
+    schedule="10 0 L * *",
+    start_date=pendulum.datetime(2024, 6, 15, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["airflow_pj"]
+    # params={"example_key": "example_value"},
+) as dag:
+    
+    # START_DATE: 전월 말일, END_DATE: 1일 전
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
+            'END_DATA':'{{ (data_interval_end.in_timezone("Asis/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}'
+            },
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+    )
+    
+    
