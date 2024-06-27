b640eb4fc7083c66f00a777704a18bcff710939d
bskim <joj1209@naver.com>
2024-06-27 16:24:51 +0900

    kwargs template

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -21,7 +21,7 @@ with DAG(
             'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}'
             },
-        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+        bash_command='echo "START_DATE_ORI: $START_DATE_ORI" && echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
     
     
