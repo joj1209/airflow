aefed515d9bb7c410c7a94cf26015977563721af
bskim <joj1209@naver.com>
2024-06-27 14:10:19 +0900

    kwargs template

--- dags/dags_python_show_template.py
+++ dags/dags_python_show_template.py
@@ -1,27 +1,20 @@
-import datetime
 import pendulum
-import random
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import task
 
 with DAG(
     dag_id="dags_python_show_template",
     schedule="30 9 * * *",
-    start_date=pendulum.datetime(2025, 6, 10, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 6, 10, tz="Asia/Seoul"),
     catchup=True,
     # dagrun_timeout=datetime.timedelta(minutes=60),
     tags=["airflow_pj"],
     # params={"example_key": "example_value"},
 ) as dag:
     
-    def select_fruit():
-        fruit = ['APPLE','BANANA','ORANGE','AVOCADO']
-        rand_init = random.randint(0,3)
-        print(fruit[rand_init])
-
-    py_t1 = PythonOperator(
-        task_id = 'py_t1',
-        python_callable=select_fruit
-    )
+    @task(task_id='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
     
-    py_t1
+    show_templates()
