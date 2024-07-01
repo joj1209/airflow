a78abc5c9f6cae13234ac46e27c456c30bc22449
bskim <joj1209@naver.com>
2024-07-01 14:38:12 +0900

    bash xcom

---
+++ dags/dags_bash_with_xcom.py
@@ -0,0 +1,34 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_xcom",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["airflow_pj","section_5"],
+    # params={"example_key": "example_value"},
+) as dag:
+    
+    bash_push = BashOperator(
+        task_id="bash_push",
+        bash_command="echo START &&"
+        "echo XCOM_PUSHED "
+        "{{ ti.xcom_push(key='bash_pushed',value='first_bash_message') }} &&"
+        "echo COMPLETE"
+    )
+    
+    bash_pull = BashOperator(
+        task_id="bash_pull",
+        env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed')}},
+                'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push')}}"},
+        bash_command="echo $PUSHED_VALUE && echo $RETRUN_VALUE"
+        do_xcom_push=False"
+    )
+    
+    bash_push >> bash_pull
+    
