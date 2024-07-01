92ed6688c4b63feeb00d3359404a5a27c9b97328
bskim <joj1209@naver.com>
2024-07-01 14:39:54 +0900

    python xcom

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -24,7 +24,7 @@ with DAG(
     
     bash_pull = BashOperator(
         task_id="bash_pull",
-        env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed')}},
+        env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed')}}",
                 'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push')}}"},
         bash_command="echo $PUSHED_VALUE && echo $RETRUN_VALUE"
         do_xcom_push=False"
