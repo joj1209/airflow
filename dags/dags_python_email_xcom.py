cf5d12852464ab721729f1517fad274f8bfae07a
bskim <joj1209@naver.com>
2024-07-01 16:18:32 +0900

    python email xcom

---
+++ dags/dags_python_email_xcom.py
@@ -0,0 +1,31 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.email import EmailOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_email_xcom",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["airflow_pj"],
+    # params={"example_key": "example_value"},
+) as dag:
+
+    @task(task_id='something_task')
+    def some_logic(**kwargs):
+        from random import choice
+        return choice(['Success','Fail'])
+    
+    send_email = EmailOperator(
+        task_id='send_email',
+        to='joj1209@naver.com',
+        subject='{{ data_interval_end.in_timezone("Asis/Seoul") |ds }} some_logic 처리결과',
+        html_content='{{ data_interval_end.in_timezone("Asia/Seoul") |ds}} 처리 결과는 <br>\
+            {{ti.xcom_pull(task_ids=something_task")}} 했습니다 <br>'
+    )
+    
+    some_logic() >> send_email
