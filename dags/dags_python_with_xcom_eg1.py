b685bf9cfb8ffad943264198f92722b4c76251b1
bskim <joj1209@naver.com>
2024-07-01 14:00:03 +0900

    python xcom

--- dags/dags_python_with_xcom_eg1.py
+++ dags/dags_python_with_xcom_eg1.py
@@ -13,13 +13,23 @@ with DAG(
 ) as dag:
     
     @task(task_id='python_xcom_push_task1')
-    def python_push1(**kwargs):
+    def xcom_push1(**kwargs):
         ti = kwargs['ti']
         ti.xcom_push(key="result1", value="value_1")
         ti.xcom_push(key="result2", value=[1,2,3])
     
     @task(task_id='python_xcom_push_task2')
-    def python_push2(**kwargs):
+    def xcom_push2(**kwargs):
         ti = kwargs['ti']
         ti.xcom_push(key="result1", value="value_2")
         ti.xcom_push(key="result2", value=[1,2,3,4])
+        
+    @task(task_id='python_xcom_push_task')
+    def xcom_pull(**kwargs):
+        ti = kwargs['ti']
+        value1 = t1.xcom_pull(key="result1")
+        value2 = t1.xcom_pull(key="result2", task_ids='python_xcom_push_task1')
+        print(value1)
+        print(value2)
+        
+    xcom_push1() >> xcom_push2() >> xcom_pull()
