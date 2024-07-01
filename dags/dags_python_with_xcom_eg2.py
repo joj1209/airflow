0252876bdf977a96516348d23078af77807ba1f6
bskim <joj1209@naver.com>
2024-07-01 14:10:55 +0900

    python xcom

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -13,15 +13,19 @@ with DAG(
 ) as dag:
     
     @task(task_id='python_xcom_push_by_return')
-    def python_push1(**kwargs):
+    def xcom_push_result(**kwargs):
         return 'Success'
     
     @task(task_id='python_xcom_pull_1')
-    def python_push2(**kwargs):
+    def xcom_pull_1(**kwargs):
         ti = kwargs['ti']
-        value1 = t1.xcom_pull(task_ids='python_xcom_push_by_return')
+        value1 = ti.xcom_pull(task_ids='python_xcom_push_by_return')
         print('xcom_pull 메서드로 직접 찾은 리턴 값:' + value1)
         
     @task(task_id='python_xcom_pull_2')
-    def python_push2(status, **kwargs):
-        print('함수 입력값으로 받은 값:' + status)     
+    def xcom_pull_2(status, **kwargs):
+        print('함수 입력값으로 받은 값:' + status)
+        
+    python_xcom_push_by_retrun = xcom_push_result()
+    xcom_pull_2(python_xcom_push_by_retrun)
+    python_xcom_push_by_retrun >> xcom_pull_1()
