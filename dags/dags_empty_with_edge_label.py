8c2788f02fce8cb2fb8634335164a591ce7722a4
bskim <joj1209@naver.com>
2024-07-05 13:44:24 +0900

    edge

---
+++ dags/dags_empty_with_edge_label.py
@@ -0,0 +1,44 @@
+import datetime
+import pendulum
+import random
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.edgemodifier import Label
+
+with DAG(
+    dag_id="dags_empty_with_edge_label",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["airflow_pj","section_6"],
+    # params={"example_key": "example_value"},
+) as dag:
+    
+    empty_1 = EmptyOperator(
+        task_id='empty_1'
+    )
+    
+    empty_2 = EmptyOperator(
+        task_id='empty_2'
+    )
+    
+    empty_1 >> Label('1과 2사이') >> empty_2
+    
+    empty_3 = EmptyOperator(
+        task_id='empty_3'
+    )
+    
+    empty_4 = EmptyOperator(
+        task_id='empty_4'
+    )
+    
+    empty_5 = EmptyOperator(
+        task_id='empty_5'
+    )
+    
+    empty_6 = EmptyOperator(
+        task_id='empty_6'
+    )
+    
+    empty_2 >> Label('Start Branch') >> [empty_3,empty_4,empty_5] >> Label('End Branch') >> empty_6
