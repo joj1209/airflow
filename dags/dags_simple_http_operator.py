f4653a8814a44da6cfff6345651c5aab5b882344
bskim <joj1209@naver.com>
2024-07-08 21:59:22 +0900

    aaaaa

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -5,7 +5,7 @@ from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_simple_http_operator",
+    dag_id="dags_simple_http_operator", 
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     catchup=False,
