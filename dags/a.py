42d8e5a2153bb5cbddebe5ec0cee53be3a306a42
bskim <joj1209@naver.com>
2024-07-16 15:59:41 +0900

    pstgres fix

--- dags/a.py
+++ dags/a.py
@@ -1,3 +1,14 @@
 import os
+from pathlib import Path
 
-print(os.urandom(16))
+# print(os.urandom(16))
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR1 = Path(__file__).resolve()
+BASE_DIR2 = Path(__file__).resolve().parent
+BASE_DIR3 = Path(__file__).resolve().parent.parent
+BASE_DIR4 = Path(__file__).resolve().parent.parent.parent
+print(BASE_DIR1)
+print(BASE_DIR2)
+print(BASE_DIR3)
+print(BASE_DIR4)
