9f56252f09d85e07dec73b1c3b898bf4751d2b62
bskim <joj1209@naver.com>
2024-07-24 15:49:23 +0900

    file_sensor

---
+++ dags/sec09/dags_file_sensor.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.sensors.filesystem import FileSensor
+import pendulum
+
+with DAG(
+    dag_id="dags_file_sensor",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["airflow_pj","section_9"],
+    # params={"example_key": "example_value"},
+) as dag:
+    
+    tvCorona19VaccinestatNew_sensor = FileSensor(
+        task_id='tvCorona19VaccinestatNew_sensor',
+        fs_conn_id='conn_file_opt_airflow_files',
+        filepath='tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/tvCorona19VaccinestatNew.csv',
+        recursive=False,
+        poke_interval=60,
+        timeout=60*60*24,
+        mode='reschedule'
+    )
