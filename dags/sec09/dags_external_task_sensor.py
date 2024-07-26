0d920df1a0abd39ec10be2ae3ab313851e0fc5ae
bskim <joj1209@naver.com>
2024-07-26 13:58:59 +0900

    external sensor

---
+++ dags/sec09/dags_external_task_sensor.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskSensor
+import Pendulum
+from datetime import timedelta
+from airflow.utils.state import State
+
+with DAG(
+    dag_id="dags_external_task_sensor",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["airflow_pj","section_9"],
+    # params={"example_key": "example_value"},
+) as dag:
+    external_task_sensor_a = ExternalTaskSensor(
+        task_id='external_task_sensor_a',
+        external_dag_id='dags_branch_python_operator',
+        external_task_id='task_a',
+        allowed_states=[State.SKIPPED],
+        execution_delta=timedelta(hour=6),
+        poke_interval=10
+    )
+    
+    external_task_sensor_b = ExternalTaskSensor(
+        task_id='external_task_sensor_b',
+        external_dag_id='dags_branch_python_operator',
+        external_task_id='task_b',
+        allowed_states=[State.SKIPPED],
+        execution_delta=timedelta(hour=6),
+        poke_interval=10
+    )
+    
+    external_task_sensor_c = ExternalTaskSensor(
+        task_id='external_task_sensor_c',
+        external_dag_id='dags_branch_python_operator',
+        external_task_id='task_c',
+        allowed_states=[State.SUCCESS],
+        execution_delta=timedelta(hour=6),
+        poke_interval=10
+    )
