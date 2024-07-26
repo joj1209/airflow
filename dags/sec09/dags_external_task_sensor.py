0b25b909ae9b9c8427accd5ee112848e583079db
bskim <joj1209@naver.com>
2024-07-26 14:06:06 +0900

    external sensor

--- dags/sec09/dags_external_task_sensor.py
+++ dags/sec09/dags_external_task_sensor.py
@@ -18,7 +18,7 @@ with DAG(
         external_dag_id='dags_branch_python_operator',
         external_task_id='task_a',
         allowed_states=[State.SKIPPED],
-        execution_delta=timedelta(hour=6),
+        execution_delta=timedelta(hours=6),
         poke_interval=10
     )
     
@@ -27,7 +27,7 @@ with DAG(
         external_dag_id='dags_branch_python_operator',
         external_task_id='task_b',
         allowed_states=[State.SKIPPED],
-        execution_delta=timedelta(hour=6),
+        execution_delta=timedelta(hours=6),
         poke_interval=10
     )
     
@@ -36,6 +36,6 @@ with DAG(
         external_dag_id='dags_branch_python_operator',
         external_task_id='task_c',
         allowed_states=[State.SUCCESS],
-        execution_delta=timedelta(hour=6),
+        execution_delta=timedelta(hours=6),
         poke_interval=10
     )
