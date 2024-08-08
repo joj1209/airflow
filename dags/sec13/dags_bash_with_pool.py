9445c450c1b519b51ec2e1127c8cdf503085464d
bskim <joj1209@naver.com>
2024-08-08 13:38:10 +0900

    pool

--- dags/sec13/dags_bash_with_pool.py
+++ dags/sec13/dags_bash_with_pool.py
@@ -15,19 +15,19 @@ with DAG(
     bash_task_1 = BashOperator(
         task_id='bask_task_1',
         bash_command='sleep 30',
-        priorty_weight=6
+        priority_weight=6
     )
     
     bash_task_2 = BashOperator(
         task_id='bask_task_2',
         bash_command='sleep 30',
-        priorty_weight=5
+        priority_weight=5
     )
     
     bash_task_3 = BashOperator(
         task_id='bask_task_3',
         bash_command='sleep 30',
-        priorty_weight=4
+        priority_weight=4
     )
     
     bash_task_4 = BashOperator(
@@ -48,17 +48,17 @@ with DAG(
     bash_task_7 = BashOperator(
         task_id='bask_task_7',
         bash_command='sleep 30',
-        priorty_weight=7
+        priority_weight=7
     )
     
     bash_task_8 = BashOperator(
         task_id='bask_task_8',
         bash_command='sleep 30',
-        priorty_weight=8
+        priority_weight=8
     )
     
     bash_task_9 = BashOperator(
         task_id='bask_task_9',
         bash_command='sleep 30',
-        priorty_weight=9
+        priority_weight=9
     )
