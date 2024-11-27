e97f9b87151898928bc6d8df3b59b53496b6f251
bskim <joj1209@naver.com>
2024-11-27 17:21:25 +0900

    mysql connect dag

--- plugins/config/get_kakao_token.py
+++ plugins/config/get_kakao_token.py
@@ -1,16 +1,16 @@
 import requests
 
-client_id = ''
+
 redirect_uri = 'https://example.com/oauth'
-authorize_code = ''
+
 
 
 token_url = 'https://kauth.kakao.com/oauth/token'
 data = {
     'grant_type': 'authorization_code',
-    'client_id': client_id,
+    # 'client_id': client_id,
     'redirect_uri': redirect_uri,
-    'code': authorize_code,
+    # 'code': authorize_code,
     }
 
 response = requests.post(token_url, data=data)
