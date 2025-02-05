6e504aa888ef3cee453975a987e17fe46e68deae
joj1209 <joj1209@naver.com>
2025-02-05 10:48:24 +0900

    sec07 move

--- dags/sec07/dags_empty_with_edge_label.py
+++ dags/sec07/dags_empty_with_edge_label.py
@@ -11,7 +11,7 @@ with DAG(
     start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     catchup=False,
     # dagrun_timeout=datetime.timedelta(minutes=60),
-    tags=["airflow_pj","section_6"],
+    tags=["airflow_pj","section_7"],
     # params={"example_key": "example_value"},
 ) as dag:
     
